82ab854840db42e1638f2e74f0dcf6eb8290a560
Christian <cbartram3@gmail.com>
2019-08-02 13:55:18 -0400

    Initial Commit

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { Menu, Image, Input, Button } from 'semantic-ui-react';
+import './Navbar.css';
+
+class Navbar extends Component {
+  render() {
+      return (
+          <Menu stackable>
+            <Menu.Item>
+              <Image alt="logo" height="30" width="30" src="https://github.com/ignitecode/react-photo/blob/part_4/src/resources/images/logo.png?raw=true" />
+            </Menu.Item>
+            <Menu.Item name="home" active>
+              Home
+            </Menu.Item>
+            <Menu.Item name="favorites">
+              Favorites
+            </Menu.Item>
+            <Menu.Item name="search">
+                <Input type="text" action  placeholder='Search'>
+                    <input />
+                    <Button type="button">Search</Button>
+                </Input>
+            </Menu.Item>
+          </Menu>
+      )
+  }
+}
+
+export default Navbar;
