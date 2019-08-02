0bc2e474001254cea7532d2fc01ea87a815e9161
Christian <cbartram3@gmail.com>
2019-08-02 13:56:40 -0400

    Added redux structure

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'semantic-ui-css/semantic.min.css'
 import './index.css';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux'; 
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
